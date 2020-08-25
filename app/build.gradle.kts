2d6ce5a88c11820c91e9fd41ff3d2be966e82eca
Kensuke Tamura <weathercock_perch@yahoo.co.jp>
2020-08-25 23:51:30 +0900

    update libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ tasks.withType<KotlinCompile>().all {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.ktor:ktor-server-netty:1.3.2")
+    implementation("io.ktor:ktor-server-netty:1.4.0")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
-    testImplementation("io.ktor:ktor-server-tests:1.3.2")
+    testImplementation("io.ktor:ktor-server-tests:1.4.0")
 }
