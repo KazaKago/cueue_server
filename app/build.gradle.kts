11ed72114e676a8db2d66f739e86d75cb3eead51
Kensuke Tamura <kazakago@gmail.com>
2020-12-08 01:18:01 +0900

    update library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     kotlin("jvm")
-    id("com.github.johnrengelman.shadow") version "6.0.0"
-    id("com.google.cloud.tools.appengine") version "2.3.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.google.cloud.tools.appengine") version "2.4.1"
 }
 
 group = "com.kazakago.ktor_starter"
