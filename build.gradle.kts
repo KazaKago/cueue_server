dc3a6e7d1d7eab4bf088e1f437412e2bd6035ce6
Kensuke Tamura <kazakago@gmail.com>
2020-12-08 17:48:37 +0900

    update kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.4.20"
-    kotlin("plugin.serialization") version "1.4.20"
+    kotlin("jvm") version "1.4.21"
+    kotlin("plugin.serialization") version "1.4.21"
 }
 
 allprojects {
