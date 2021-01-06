73e735e7abb36a17d42e6caa849f723f9d1ba404
Kensuke Tamura <kazakago@gmail.com>
2021-01-06 18:53:06 +0900

    library update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("io.ktor:ktor-server-netty:1.4.3")
-    implementation("io.ktor:ktor-serialization:1.4.3")
+    implementation("io.ktor:ktor-server-netty:1.5.0")
+    implementation("io.ktor:ktor-serialization:1.5.0")
     implementation("com.google.cloud:google-cloud-firestore:1.32.0")
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
