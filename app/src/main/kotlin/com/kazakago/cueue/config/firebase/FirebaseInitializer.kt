dc05f93683d63348aff062ab7fd52597d03a63ed
Kensuke Tamura <kazakago@gmail.com>
2021-01-29 15:21:33 +0900

    fix environment

--- app/src/main/kotlin/com/kazakago/cueue/config/firebase/FirebaseInitializer.kt
+++ app/src/main/kotlin/com/kazakago/cueue/config/firebase/FirebaseInitializer.kt
@@ -7,7 +7,7 @@ import io.ktor.application.*
 import java.io.FileInputStream
 
 fun Application.initializeFirebase() {
-    val serviceAccount = FileInputStream("app/cueue-8ad42-firebase-adminsdk-8jekl-940ada59c9.json")
+    val serviceAccount = FileInputStream(environment.config.property("app.google.credentials").getString())
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(serviceAccount))
         .build()
