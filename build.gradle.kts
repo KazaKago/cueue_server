f6ca856afec2695c987c5db94d24ff447d58d621
Kensuke Tamura <kazakago@gmail.com>
2021-02-04 12:13:38 +0900

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.serialization") version "1.4.21"
+    kotlin("jvm") version "1.4.30"
+    kotlin("plugin.serialization") version "1.4.30"
 }
 
 allprojects {
