004c2a7d0cffae310cdebc896a1638914dff6dfc
Kensuke Tamura <kazakago@gmail.com>
2021-02-11 16:29:17 +0900

    fix for signinwithapple

--- app/src/main/kotlin/com/kazakago/cueue/model/FirebaseUser.kt
+++ app/src/main/kotlin/com/kazakago/cueue/model/FirebaseUser.kt
@@ -5,7 +5,7 @@ import java.net.URL
 
 data class FirebaseUser(
     val uid: UID,
-    val name: String,
+    val name: String?,
     val picture: URL?,
     val email: Email,
     val isEmailVerified: Boolean
