92bc088ef7d6eea22c02ad4cc99fb1a67f71a18b
Kensuke Tamura <kazakago@gmail.com>
2021-04-09 22:09:22 +0900

    update libraries.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     application
     kotlin("jvm")
     kotlin("plugin.serialization")
-    id("org.flywaydb.flyway") version "7.5.4"
+    id("org.flywaydb.flyway") version "7.7.3"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
@@ -12,7 +12,7 @@ group = "com.kazakago.cueue"
 version = "1.0.0-SNAPSHOT"
 
 application {
-    mainClassName = "io.ktor.server.netty.EngineMain"
+    mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
 java {
@@ -38,21 +38,21 @@ tasks.register("stage") {
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.ktor:ktor-server-netty:1.5.2")
-    implementation("io.ktor:ktor-serialization:1.5.2")
-    implementation("io.ktor:ktor-auth:1.5.2")
+    implementation("io.ktor:ktor-server-netty:1.5.3")
+    implementation("io.ktor:ktor-serialization:1.5.3")
+    implementation("io.ktor:ktor-auth:1.5.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
-    implementation("org.jetbrains.exposed:exposed-core:0.29.1")
-    implementation("org.jetbrains.exposed:exposed-dao:0.29.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.29.1")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.29.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.30.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.30.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.30.1")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.30.1")
     implementation("org.postgresql:postgresql:42.2.19")
-    implementation("org.flywaydb:flyway-core:7.5.4")
+    implementation("org.flywaydb:flyway-core:7.7.3")
     implementation("org.koin:koin-ktor:2.2.2")
-    implementation("com.google.firebase:firebase-admin:7.1.0")
+    implementation("com.google.firebase:firebase-admin:7.1.1")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("org.sejda.imageio:webp-imageio:0.1.6")
     implementation("com.drewnoakes:metadata-extractor:2.15.0")
 
-    testImplementation("io.ktor:ktor-server-tests:1.4.3")
+    testImplementation("io.ktor:ktor-server-tests:1.5.3")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    kotlin("jvm") version "1.4.31"
-    kotlin("plugin.serialization") version "1.4.31"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.serialization") version "1.4.32"
 }
 
 allprojects {
     repositories {
+        mavenCentral()
         jcenter()
     }
 }
