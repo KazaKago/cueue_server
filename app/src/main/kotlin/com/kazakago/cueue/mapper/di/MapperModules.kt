691b11c0bc066d1d29093d63a4505101cb90c068
Kensuke Tamura <kazakago@gmail.com>
2021-06-12 18:20:19 +0900

    update libraries.

--- app/src/main/kotlin/com/kazakago/cueue/mapper/di/MapperModules.kt
+++ app/src/main/kotlin/com/kazakago/cueue/mapper/di/MapperModules.kt
@@ -7,8 +7,9 @@ val mapperModules = module {
     single { UserMapper(get()) }
     single { WorkspaceMapper() }
     single { RecipeMapper(get()) }
+    single { RecipeSummaryMapper() }
     single { TagMapper() }
     single { MenuMapper(get(), get()) }
+    single { MenuSummaryMapper(get(), get()) }
     single { TimeFrameMapper() }
-    single { RecipeSummaryMapper() }
 }
