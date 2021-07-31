0179728b1b5d07183b85f2e31ec7279ab4a35f74
KazaKago <kazakago@gmail.com>
2021-07-31 18:25:46 +0900

    update kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.5.10"
-    kotlin("plugin.serialization") version "1.5.10"
+    kotlin("jvm") version "1.5.21"
+    kotlin("plugin.serialization") version "1.5.21"
 }
 
 allprojects {
