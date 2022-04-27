3794c7df0da3086a700596ab22edc5c90df6d92b
Kensuke Tamura <kazakago@gmail.com>
2022-04-27 09:08:05 +0900

    add pluginmanagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
