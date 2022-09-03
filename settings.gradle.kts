d8710f3c036f3df72762af0e0be5748e3b052bd1
Kensuke Tamura <kazakago@gmail.com>
2022-09-04 00:44:17 +0900

    add suspender module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
 rootProject.name = "cueue"
 include("server")
 include("worker")
+include("suspender")
 
 // https://docs.gradle.org/current/userguide/declaring_dependencies.html#sec:type-safe-project-accessors
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
---
+++ suspender/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+@Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
+plugins {
+    application
+    alias(libs.plugins.kotlin.jvm)
+}
+
+group = "com.kazakago.cueue.suspender"
+version = libs.versions.version.get()
+
+application {
+    mainClass.set("com.kazakago.cueue.suspender.SuspenderKt")
+}
+
+java {
+    setSourceCompatibility(libs.versions.java.get())
+    setTargetCompatibility(libs.versions.java.get())
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = libs.versions.java.get()
+}
+
+dependencies {
+    implementation(libs.picocli)
+    implementation(libs.typesafe.config)
+    implementation(libs.okHttp)
+}
